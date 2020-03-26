4539bb0cf968e75754c82ea4b38cfad00c5c943c
andrigamerita <37557992+andrigamerita@users.noreply.github.com>
2020-03-26 12:21:03 +0100

    1

---
+++ eng/sources/lanfiles_download.bat
@@ -0,0 +1,39 @@
+@echo off
+title LanPlayHelper Files Downloader
+color 9f
+
+echo.
+echo.
+echo.
+echo *      *****  **   *  *****  *      *****  *   *
+echo *      *   *  * *  *  *   *  *      *   *   * *
+echo *      *****  * ** *  *****  *      *****    *
+echo *      *   *  *  * *  *      *      *   *    *
+echo *****  *   *  *   **  *      *****  *   *    *
+echo.
+echo     *   *  *****  *      *****  *****  *****
+echo     *   *  *      *      *   *  *      *   *
+echo     *****  *****  *      *****  *****  *****
+echo     *   *  *      *      *      *      *  *
+echo     *   *  *****  *****  *      *****  *   *
+echo.
+echo.
+echo.
+
+mkdir lanhelperfiles
+echo A "lanhelperfiles" folder has been created in
+echo the same directory this script resides in:
+echo %cd%
+echo.
+echo Files needed for LanPlayHelper
+echo will now be downloaded in it.
+echo Be sure to have an active internet connection.
+echo.
+echo Press Enter to start the download.
+pause > nul
+
+cd lanhelperfiles
+wget -O 
+
+cls
+echo spacc completed

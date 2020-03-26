14295333a70bc12e036edaeb8b3708f2fa17cf4d
andrigamerita <37557992+andrigamerita@users.noreply.github.com>
2020-03-26 13:02:41 +0100

    3

--- downloader_ENG.bat
+++ downloader_ENG.bat
@@ -1,6 +1,5 @@
 @echo off
-cmd
-@echo off
+cls
 title LanPlayHelper Files Downloader
 color 9f
 
@@ -22,8 +21,8 @@ echo.
 echo.
 echo.
 
-mkdir lanhelperfiles
-echo A "lanhelperfiles" folder has been created in
+mkdir LanPlayHelper
+echo A "LanPlayHelper" folder has been created in
 echo the same directory this script resides in:
 echo %cd%
 echo.
@@ -34,8 +33,22 @@ echo.
 echo Press Enter to start the download.
 pause > nul
 
-cd lanhelperfiles
-bitsadmin /transfer lan-play /priority high UrlOfTheFile SaveFileAsName
+cd LanPlayHelper
+mkdir stuff
+bitsadmin /transfer main /priority high https://github.com/andrigamerita/ACNH-LanPlayHelper/blob/master/sources/start_ENG.bat %cd%\start_ENG.bat
+cd stuff
+bitsadmin /transfer lanplay /priority high https://github.com/andrigamerita/ACNH-LanPlayHelper/raw/master/sources/lan-play.exe %cd%\lan-play.exe
+bitsadmin /transfer winpcap /priority high https://github.com/andrigamerita/ACNH-LanPlayHelper/blob/master/sources/winpcap-setup.exe %cd%\winpcap-setup.exe
+cd ..
 
 cls
-echo spacc completed
+echo.
+echo.
+echo.
+echo The needed files have been downloaded in:
+echo %cd%
+echo.
+echo Press Enter to close this script and open the folder.
+pause > nul
+explorer %cd%
+exit
---
+++ sources/start_ENG.bat
@@ -0,0 +1,13 @@
+@echo off
+title ACNH LanPlayHelper
+color 9f
+
+echo.
+echo.
+echo.
+echo Welcome to..
+echo.
+echo ACNH LanPlayHelper
+echo A tool designed to make playing ACNH with lanplay easy.
+
+pause > nul
